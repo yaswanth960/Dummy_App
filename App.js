3c930bc451468d77f2819c07ceb3704b9ef44fa0
yaswanth960 <yaswanthramisetty99@gmail.com>
2022-03-08 16:26:39 +0530

    event changed

--- App.js
+++ App.js
@@ -1,115 +1,128 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Alert, StyleSheet, Text, View, TouchableOpacity} from 'react-native';
-import {Agenda} from 'react-native-calendars';
+import {Picker} from '@react-native-picker/picker';
+import {Agenda, Calendar} from 'react-native-calendars';
+import {meeting} from './data/meetings';
 
 const App = () => {
-  let data = {
-    '2022-03-10': [
-      {
-        name: '10.00 AM - 10.45 AM',
-        name1: 'Yaswanth Ramisetty',
-        name2: 'Software Engineer',
-        name3: 'S',
-      },
-    ],
-    '2022-03-11': [],
-    '2022-03-12': [
-      {
-        name: '11.00 AM - 11.30 AM',
-        name1: 'Sai Chand',
-        name2: 'Devops Engineer',
-        name3: 'D',
-      },
-    ],
-    '2022-03-13': [],
-    '2022-03-14': [],
-    '2022-03-15': [
-      {
-        name: '11.00 AM - 11.30 AM',
-        name1: 'Sandeep Varma',
-        name2: 'Business Man',
-        name3: 'S',
-      },
-    ],
-    '2022-03-16': [],
-    '2022-03-23': [
-      {
-        name: '10.00 PM - 10.45 PM',
-        name1: 'Kiran Kumar',
-        name2: 'Relience Manager',
-        name3: 'K',
-      },
-    ],
-    '2022-03-24': [],
-    '2022-03-25': [
-      {
-        name: '11.00 AM - 11.30 AM',
-        name1: 'Suneel Kumar',
-        name2: 'Manager in More',
-        name3: 'S',
-      },
-    ],
-    '2022-03-26': [],
-    '2022-03-27': [
-      {
-        name: '11.00 AM - 11.30 AM',
-        name1: 'Sai Chand',
-        name2: 'Devops Engineer',
-        name3: 'D',
-      },
-    ],
+  const [selectedvalue, setSelectedValue] = useState('j');
+  const [datas, setDatas] = useState({});
+
+  React.useEffect(() => {
+    let trans_date = {};
+    meeting.forEach(meet => {
+      let d = meet.date;
+      if (trans_date[d]) {
+        trans_date[d].push(meet);
+      } else {
+        trans_date[d] = [meet];
+      }
+    });
+    setDatas(trans_date);
+  }, []);
+
+  const setStatus = (date, firstItemInDay, sta) => {
+    let obj = {
+      ...datas,
+      [date]: datas[date].map((item, index) =>
+        index == 0 ? {...item, status: sta} : item,
+      ),
+    };
+    setDatas(obj);
   };
 
-  const renderItems = item => {
+  const renderItems = (item, firstItemInDay, day) => {
+    let a = item.name1.split(' ');
+    let a1 = a[0][0];
+    let a2 = a[1][0] != undefined ? a[1][0] : '';
+
     return (
       <View
         style={{
           flexDirection: 'row',
           justifyContent: 'space-between',
-          backgroundColor: 'white',
+          backgroundColor:
+            item.status == 'Default'
+              ? 'white'
+              : item.status == 'Sucess'
+              ? '#b6d4be'
+              : item.status == 'Pending'
+              ? '#e0908d'
+              : '#d6c685',
           height: 100,
+          borderRadius: 10,
           padding: 20,
+          marginRight: 13,
           marginTop: 10,
         }}>
         <View>
           <Text>{item.name}</Text>
           <Text style={{fontWeight: 'bold', fontSize: 17}}>{item.name1}</Text>
-          <Text>{item.name2}</Text>
+          <View>
+            <Picker
+              selectedValue={item.status}
+              onValueChange={select =>
+                setStatus(item.date, firstItemInDay, select)
+              }
+              style={{height: 20, width: 150, marginBottom: 20}}>
+              <Picker.Item label="Default" value="Default" />
+              <Picker.Item label="Sucess" value="Sucess" />
+              <Picker.Item label="Pending" value="Pending" />
+              <Picker.Item label="Hold" value="Hold" />
+            </Picker>
+          </View>
         </View>
+
         <View
           style={{
             justifyContent: 'center',
             height: 50,
             width: 50,
-            backgroundColor: '#d4b4b2',
+            backgroundColor:
+              a1 <= 'E'
+                ? '#d4b4b2'
+                : a1 <= 'K'
+                ? '#4cd470'
+                : a1 <= 'M'
+                ? '#c98fcf'
+                : '#cce3e0',
             borderRadius: 25,
             alignItems: 'center',
             marginTop: 5,
           }}>
-          <Text style={{fontWeight: 'bold', fontSize: 20}}>{item.name3}</Text>
+          <Text style={{fontWeight: 'bold', fontSize: 20}}>{a1 + a2}</Text>
         </View>
       </View>
     );
   };
 
+  const renderEmptyDataa = () => {
+    return (
+      <View
+        style={{justifyContent: 'center', alignItems: 'center', height: 100}}>
+        <Text style={{color: 'black', fontWeight: 'bold'}}>
+          No Event Available Here
+        </Text>
+      </View>
+    );
+  };
 
   return (
     <View style={{flex: 1}}>
       <Agenda
-      theme={{
-        
-        agendaDayTextColor: '#0f0c0b',
-        agendaDayNumColor: 'green',
-        agendaTodayColor: 'red',
-        agendaKnobColor: 'blue'
-      }}
-        items={data}
+        theme={{
+          agendaDayTextColor: '#0f0c0b',
+          agendaDayNumColor: 'green',
+          agendaTodayColor: 'red',
+          agendaKnobColor: 'blue',
+        }}
+        items={datas}
         selected={new Date()}
         minDate={'2022-02-03'}
+        renderEmptyData={renderEmptyDataa}
         renderItem={renderItems}
         maxDate={'2022-07-30'}
         pastScrollRange={50}
-        futureScrollRange={50}
       />
     </View>
   );
